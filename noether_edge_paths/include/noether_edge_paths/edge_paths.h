df733ebf9210c7e150745fda87e8f041fac0dd7f
dmerz <david.merz@swri.org>
2019-10-16 13:52:29 -0500

    edge paths

--- noether_edge_paths/include/noether_edge_paths/edge_paths.h
+++ noether_edge_paths/include/noether_edge_paths/edge_paths.h
@@ -1,17 +1,64 @@
 #ifndef EDGE_PATHS_H
 #define EDGE_PATHS_H
 
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/PolygonMesh.h>
+
+#include <noether_msgs/ToolPathConfig.h>
+#include <noether_msgs/ToolRasterPath.h>
+
+#include <robust_edge_extraction/robust_edge_extraction.h>
+#include <pcl_bspline_reconstruction/bspline_reconstruction.h>
+
 namespace noether_edge_paths
 {
 
 class EdgePathGenerator
 {
 public:
+  EdgePathGenerator();
+
+  void setInputCloud(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& input_cloud);
+  void setInputMesh(const pcl::PolygonMesh& input_mesh);
+
+  void setResolution(const double resolution) { resolution_ = resolution; }
+  void setNumberNeighbors(const int num_neighbors) { num_neighbors_ = num_neighbors; }
+  void setInteriorPathAngle(const double in_degrees_from_normal) { interior_path_angle_ = in_degrees_from_normal; }
+  void setExteriorPathAngle(const double in_degrees_from_normal) { exterior_path_angle_ = in_degrees_from_normal; }
+  void setToolPathParams(const noether_msgs::ToolPathConfig& config) { config_ = config; }
+
+  /**
+   * @brief computeEdgePaths - computes paths which roughly follow the
+   * outline of the supplied mesh/point cloud.  They break on hard corners
+   * so that speed constraints may be more easily met.
+   * @param paths - output - PoseArray[], an array of arrays of poses.
+   * Each PoseArray will represent one stroke along one continuous line.
+   * They will break on hard corners.
+   */
+  void computeEdgePaths(noether_msgs::ToolRasterPath& paths);
 
 private:
+  // Sorts points into not-edges, edges, and corners
+  void evaluateEdginess(const pcl::PointCloud<pcl::PointXYZHSV>::ConstPtr& input_cloud, pcl::PointCloud<pcl::PointXYZI>::ConstPtr output_cloud);
+
+  robust_edge_extraction::RobustEdgeExtraction edge_detector_;
+  bspline_reconstruction::BSplineReconstruction bspline_generator_;
+
+  pcl::PointCloud<pcl::PointXYZ>::ConstPtr input_cloud_;
+  pcl::PointCloud<pcl::PointXYZHSV>::ConstPtr edge_results_;
+
+  double resolution_;
+  double search_radius_;
+  int num_neighbors_;
+  double interior_path_angle_;
+  double exterior_path_angle_;
+  noether_msgs::ToolPathConfig config_;
 
 }; // end class EdgePathGenerator
 
 } // end namespace noether_edge_paths
+
+// How to decide to break one edge from the next?
 
 #endif // EDGE_PATHS_H
--- noether_edge_paths/src/edge_paths.cpp
+++ noether_edge_paths/src/edge_paths.cpp
@@ -1,28 +1,72 @@
+#include <noether_edge_paths/edge_paths.h>
+
+#include <pcl/conversions.h>
+
 namespace noether_edge_paths
 {
 
-// Set Input Mesh (raw)
-// Set Input Cloud (raw)
-// Set Input Mesh (already PCAd)
-// Set Input Cloud (already PCAd)
+EdgePathGenerator::EdgePathGenerator()
+{
+  input_cloud_.reset(new pcl::PointCloud<pcl::PointXYZ>());
 
-// Set Numerical Parameters
-  // Search Radius / Number of Neighbors
-  // Where to break one edge from the next
-  // Path Standoff
-  // Exterior Angle
-  // Interior Angle
-  // Waypoint Spacing
+  resolution_ = 0.01;
+  num_neighbors_ = 20;
+  search_radius_ = 0.05;
+  interior_path_angle_ = 0.0;
+  exterior_path_angle_ = 0.0;
+  config_.pt_spacing = 0.1;
+  config_.tool_offset = 0.0;
+}
 
-// If necessary, do the PCA
+void EdgePathGenerator::setInputCloud(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& input_cloud)
+{
+  input_cloud_ = input_cloud;
+  return;
+}
 
-// Calculate Paths
+void EdgePathGenerator::setInputMesh(const pcl::PolygonMesh& input_mesh)
+{
+  pcl::PointCloud<pcl::PointXYZ> input_cloud;
+  pcl::fromPCLPointCloud2(input_mesh.cloud, input_cloud);
+  input_cloud_.reset(new pcl::PointCloud<pcl::PointXYZ>(input_cloud));
+  return;
+}
+
+void EdgePathGenerator::computeEdgePaths(noether_msgs::ToolRasterPath& paths)
+{
+  paths.paths.clear();
+
+  // Calculate Paths
+  // Do the PCA
+  edge_detector_.setInputCloud(input_cloud_, resolution_);
+  edge_detector_.solve(search_radius_, num_neighbors_);
+  edge_results_ = edge_detector_.getEdgeResults();
+
   // Evaluate the PCA results to identify edges
   // Extract the edges from the mesh/cloud
+  pcl::PointCloud<pcl::PointXYZI>::ConstPtr edginess_cloud;
+  evaluateEdginess(edge_results_, edginess_cloud);
+
+  // Separate the continuous edge segments
+    // - some manner of region growing?
+    // Use connectivity in half-edge mesh to add to list before calling next one
   // Identify the external edge (longest / most points?)
-  // Separate the edge segments somehow (convex decomposition?)
+    // Connectivity of half-edge mesh - which has the most half-edge segments in it?
+    // Or just most points.
+    // Either way assumes even point spacing, which may not be correct.
+    // Could actually calculate the distance of each segment to get more accurate number.
+    // Could calculate the distance from each point to 3-4 points back in order to smooth the distance number.
+  // Separate each individual edge 'side'
+
   // Fit B-Splines to each edge group
+    // Collection of points / submesh
+    // calculate general/average normal
   // Place evenly spaced waypoints along each B-Spline
+    // It's parameterized, so that's cool.
   // Rotate the b-spline points, then apply standoff
+    // rotate relative to existing e
+
+  return;
+}
 
 } // end namespace noether_edge_paths
